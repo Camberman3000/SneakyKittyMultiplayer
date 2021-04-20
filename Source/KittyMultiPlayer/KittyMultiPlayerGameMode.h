95891ce1f333aa152efe0b32681efd5283372d94
Mark <camberman3000@hotmail.com>
2021-04-20 08:25:45 -0400

    Initial commit

---
+++ Source/KittyMultiPlayer/KittyMultiPlayerGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "KittyMultiPlayerGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AKittyMultiPlayerGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AKittyMultiPlayerGameMode();
+};
+
+
+
